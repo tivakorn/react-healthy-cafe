40f7b37db4dcb3f3924e9269b63ade09df0d0c75
Tivakorn <not_not_not1@hotmail.co.th>
2019-08-05 03:39:54 +0700

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { Component } from 'react';
+import Header from './components/Header'
+import Monitor from './components/monitor/Monitor'
+import Footer from './components/Footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { products: "" }
+  }
+
+  componentDidMount() {
+    this.setState({
+      products: [
+        { productId: 1, productName: "สลัดผัก", unitPrice: "120", thumbnail: "/images/product/1.jpg" },
+        { productId: 2, productName: "ไก่ทอด", unitPrice: "90", thumbnail: "/images/product/2.jpg" },
+        { productId: 3, productName: "บิงซู", unitPrice: "200", thumbnail: "/images/product/3.jpg" },
+        { productId: 4, productName: "เฟรนฟราย", unitPrice: "140", thumbnail: "/images/product/4.jpg" },
+        { productId: 5, productName: "เค้ก 3 ชั้น", unitPrice: "200", thumbnail: "/images/product/5.jpg" },
+        { productId: 6, productName: "กาแฟ เฮลตี้ฟู้ด", unitPrice: "140", thumbnail: "/images/product/6.jpg" }
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Monitor products={this.state.products} />
+        <Footer company="DISCONNECT" email="dis@connect.com" />
+      </div>
+    )
+  }
 }
 
 export default App;
